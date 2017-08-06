dcbc4823110e034f624535061b66c2f94fa222f4
Kyle Lamse <kyle.lamse@cgi.com>
2017-08-06 18:29:29 -0500

    Global styles

---
+++ components/global.ts
@@ -0,0 +1,7 @@
+import { css, fontFace } from 'glamor';
+
+css.global('html, body', {
+    fontFamily: 'Roboto, sans-serif',
+    padding: '0',
+    margin: 0
+});
--- stories/index.tsx
+++ stories/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 import { Button } from '../components';
+import '../components/global.ts';
 
 storiesOf('Button', module)
     .add('Default', () =>
