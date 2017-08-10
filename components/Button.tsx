7ef2957bc5b177667bfaa92665da3b7f55464817
Kyle Lamse <kyle.lamse@cgi.com>
2017-08-10 16:47:59 -0500

    Everything besides @keyframes in place

--- components/Button.tsx
+++ components/Button.tsx
@@ -23,6 +23,51 @@ const shadow8DP: string = `0px 5px 5px -3px rgba(0, 0, 0, 0.2), \
 // easing
 const easeInOut: string = 'cubic-bezier(0.4, 0.0, 0.2, 1)';
 
+// disable styles
+const disabledStyles: any = {
+    opacity: 0.4
+};
+
+// enabled styles
+const rippleDimension: number = 100;
+const marginOffset: number = - (rippleDimension / 2);
+const enabledStyles: any = {
+    // hover styles
+    [':hover']: {
+        cursor: 'pointer',
+        background: focusedColor
+    },
+    // during click styles
+    [':active']: {
+        boxShadow: shadow8DP
+    },
+    // ripple styles
+    [':after']: {
+        visiblity: 'hidden',
+        content: '',
+        display: 'block',
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        borderRadius: '50%',
+        background: 'white',
+        height: `${rippleDimension}px`,
+        width: `${rippleDimension}px`,
+        marginLeft: `${marginOffset}px`,
+        marginTop: `${marginOffset}px`,
+        transform: 'scale(0)'
+    },
+    // ripple after clicked styles
+    [':not(:active):after']: {
+        animation: `ripple 1000ms ${easeInOut}`
+    },
+    // only allow animation when focused
+    [':focus:after']: {
+        visiblity: 'visibile'
+    }
+};
+
+// base styles
 const ButtonContainer: any = button({
     fontFamily: '\'Roboto\', sans-serif',
     fontWeight: 500,
@@ -40,22 +85,13 @@ const ButtonContainer: any = button({
     borderRadius: '2px',
     color: textColor,
     background: restingColor,
-    boxShadow: shadow2DP
+    boxShadow: shadow2DP,
+    transition: `all 400ms ${easeInOut}`
 },
-({disabled = false}: Props) => {
-    if (disabled) {
-        return {
-            opacity: 0.4
-        };
-    } else {
-        return {
-            ':hover': {
-                cursor: 'pointer',
-                background: focusedColor
-            }
-        };
-    }
-});
+// function to conditionally apply style objects based on props
+({disabled = false}: Props) => (
+    disabled ? disabledStyles : enabledStyles
+));
 
 interface Props {
     children: string;
