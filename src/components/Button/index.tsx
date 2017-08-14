59332f56e0113088100d362e49620bfd0fc3572d
Kyle Lamse <Kyle_Lamse@baylor.edu>
2017-08-14 18:35:18 -0500

    More moving files around

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,3 @@
+export { FlatButton } from './FlatButton';
+export { FloatingActionButton} from './FloatingActionButton';
+export { RaisedButton } from './RaisedButton';
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,3 @@
 import * as React from 'react';
 
-export { RaisedButton } from './Button/RaisedButton';
-export { FlatButton } from './Button/FlatButton';
-export { FloatingActionButton } from './Button/FloatingActionButton';
+export { RaisedButton, FlatButton, FloatingActionButton } from './Button';
