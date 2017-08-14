d6606e26a2e5ced9e02a7e1841d5ae0414ed44dd
Kyle Lamse <Kyle_Lamse@baylor.edu>
2017-08-14 18:51:17 -0500

    Remove semicolon from font string

--- src/global.ts
+++ src/global.ts
@@ -12,13 +12,13 @@ css.global('html, body', {
 const reg: string = `local('Roboto'), local('Roboto-Regular'), url(\
 https://fonts.gstatic.com/s/roboto/v16/CWB0XYA8bzo0kSThX0UTuA.woff2)\
 format('woff2');unicode-range: U+0000-00FF, U+0131, U+0152-0153,\
-U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2212, U+2215;`;
+U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2212, U+2215`;
 
 const med: string = `local('Roboto Medium'), \
 local('Roboto-Medium'), url(https://fonts.gstatic.com/s/roboto/\
 v16/RxZJdnzeo3R5zSexge8UUVtXRa8TVwTICgirnJhmVJw.woff2) format(\'woff2\');\
 unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02C6, U+02DA, U+02DC, \
-U+2000-206F, U+2074, U+20AC, U+2212, U+2215;`;
+U+2000-206F, U+2074, U+20AC, U+2212, U+2215`;
 css.global('@font-face', {
     fontFamily: 'Roboto',
     fontStyle: 'normal',
