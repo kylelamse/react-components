1e12a493933a5979d3f94e443dd03be19a905cfa
Kyle Lamse <kylelamse29@gmail.com>
2017-08-14 20:48:52 -0500

    Omg lol

--- src/typings/index.ts
+++ src/typings/index.ts
@@ -1,6 +1,6 @@
-import * as React from 'react';
+import { HTMLProps } from 'react';
 import { GlamorousComponent } from 'glamorous';
 
 export declare type StyledButton<Props> = GlamorousComponent<
-    React.HTMLProps<HTMLButtonElement> & Props, Props
+    HTMLProps<HTMLButtonElement> & Props, Props
 >;
